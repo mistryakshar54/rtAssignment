e4ea81969346ea416cdbe4b220c1bcc148ca4bb9
mistryakshar54 <mistry.aks@gmail.com>
2016-08-24 03:13:28 +0530

    added index

--- login.php
+++ login.php
@@ -1,18 +1,24 @@
 <?php
-session_start();
-require_once __DIR__ . '/facebook-sdk-v5/autoload.php';
+// session_start();
+// require_once __DIR__ . '/facebook-sdk-v5/autoload.php';
 
-$fb = new Facebook\Facebook([
-  'app_id' => '645064765651171',
-  'app_secret' => '78fe59553f88546e1e54c6c4adb2a8f2',
-  'default_graph_version' => 'v2.5',
-]);
+// $fb = new Facebook\Facebook([
+//   'app_id' => '645064765651171',
+//   'app_secret' => '78fe59553f88546e1e54c6c4adb2a8f2',
+//   'default_graph_version' => 'v2.5',
+// ]);
 
-$helper = $fb->getRedirectLoginHelper();
-$permissions = ['email', 'user_likes']; // optional
-$loginUrl = $helper->getLoginUrl('http://aksharmistry.coolpage.biz/login-callback.php', $permissions);
+// $helper = $fb->getRedirectLoginHelper();
+// $permissions = ['email', 'user_likes']; // optional
+// $loginUrl = $helper->getLoginUrl('http://aksharmistry.coolpage.biz/login-callback.php', $permissions);
 
-echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
+// echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
 
 
 ?>
+
+<html>
+	<body>
+		hello
+	</body>
+</html>
